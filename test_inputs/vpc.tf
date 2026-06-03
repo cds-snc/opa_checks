540d23b52b32d62c98757e9fb3e96c4c809c9f00
Max Neuvians <max.neuvians@cds-snc.ca>
2026-06-03 09:33:21 -0400

    fix: deps (#59)

--- test_inputs/vpc.tf
+++ test_inputs/vpc.tf
@@ -1,7 +1,8 @@
-module "vpc" { 
-  source = "github.com/cds-snc/terraform-modules//vpc"
-  name = "vpc"
-  billing_tag_value = "cal"
-  high_availability = true
-  enable_eip = false
+module "vpc" {
+  source             = "github.com/cds-snc/terraform-modules//vpc"
+  name               = "vpc"
+  billing_tag_value  = "cal"
+  availability_zones = 3
+  cidrsubnet_newbits = 8
+  enable_eip         = false
 }
